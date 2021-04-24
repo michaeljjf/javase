10609ee836fe91429d6c4ae878b021bf08f5be07
jiangjf <34030327@qq.com>
2021-04-24 22:30:14 +0800

    流程控制学习

---
+++ src/com/michaeljjf/chapter4/TestFor08.java
@@ -0,0 +1,15 @@
+package com.michaeljjf.chapter4;
+
+public class TestFor08 {
+    public static void main(String[] args) {
+        //return:遇到return结束当前正在执行的方法
+        for (int i = 1; i <= 100; i++) {
+            while (i == 36) {
+                return;
+            }
+            System.out.println(i);
+        }
+
+        System.out.println("-----");
+    }
+}
