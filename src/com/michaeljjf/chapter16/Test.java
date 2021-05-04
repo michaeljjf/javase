94b5fdd3f01f30c9446387982f6e3a7ef9159c90
jiangjf <34030327@qq.com>
2021-05-04 21:41:07 +0800

    测试代码

--- src/com/michaeljjf/chapter16/Test.java
+++ src/com/michaeljjf/chapter16/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         Person person = new Person();
         System.out.println(person.toString());
+        System.out.println(Math.random());
     }
 }
